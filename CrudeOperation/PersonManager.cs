fa2c79910d128d626c5924d0168a956326c3230e
junaiyetmia470@gmail.com <rezowan.7890@gmail.com>
2024-08-25 16:45:29 +0600

    fgfgg

---
+++ CrudeOperation/PersonManager.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PersonManager
+{
+    private List<Person> _people = new List<Person>();
+    private int _nextId = 1;
+
+    // Create
+    public Person Create(string name, int age)
+    {
+        var person = new Person
+        {
+            Id = _nextId++,
+            Name = name,
+            Age = age
+        };
+        _people.Add(person);
+        return person;
+    }
+
+    // Read
+    public Person Read(int id)
+    {
+        return _people.FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<Person> ReadAll()
+    {
+        return _people;
+    }
+
+    // Update
+    public bool Update(int id, string name, int age)
+    {
+        var person = _people.FirstOrDefault(p => p.Id == id);
+        if (person != null)
+        {
+            person.Name = name;
+            person.Age = age;
+            return true;
+        }
+        return false;
+    }
+
+    // Delete
+    public bool Delete(int id)
+    {
+        var person = _people.FirstOrDefault(p => p.Id == id);
+        if (person != null)
+        {
+            _people.Remove(person);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ CrudeOperation/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        var manager = new PersonManager();
+
+        // Create
+        var person1 = manager.Create("Alice", 30);
+        var person2 = manager.Create("Bob", 25);
+
+        // Read
+        Console.WriteLine("All people:");
+        foreach (var person in manager.ReadAll())
+        {
+            Console.WriteLine($"Id: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+        }
+
+        // Update
+        manager.Update(person1.Id, "Alicia", 31);
+        var updatedPerson = manager.Read(person1.Id);
+        Console.WriteLine($"\nUpdated person: Id: {updatedPerson.Id}, Name: {updatedPerson.Name}, Age: {updatedPerson.Age}");
+
+        // Delete
+        manager.Delete(person2.Id);
+
+        // Read all after deletion
+        Console.WriteLine("\nAll people after deletion:");
+        foreach (var person in manager.ReadAll())
+        {
+            Console.WriteLine($"Id: {person.Id}, Name: {person.Name}, Age: {person.Age}");
+        }
+    }
+}
